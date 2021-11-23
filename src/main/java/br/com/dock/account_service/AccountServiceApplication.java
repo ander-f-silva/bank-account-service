8b64d9c75c483af4175f9af94f1a7b068a35fb4e
andferreira <anderson.ferreira@mercadolivre.com>
2021-11-23 20:06:26 -0300

    feat: Format the files .java

--- src/main/java/br/com/dock/account_service/AccountServiceApplication.java
+++ src/main/java/br/com/dock/account_service/AccountServiceApplication.java
@@ -1,4 +1,4 @@
-package br.com.dock.accountservice;
+package br.com.dock.account_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/dock/account_service/AccountServiceApplicationTests.java
+++ src/test/java/br/com/dock/account_service/AccountServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.dock.accountservice;
+package br.com.dock.account_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
