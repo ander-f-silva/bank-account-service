28635634ece98754ebf08c0ff1d371ad6e89988d
andferreira <anderson.ferreira@mercadolivre.com>
2021-11-25 22:57:19 -0300

    feat: Add the new endpoint to account statement

--- src/test/java/br/com/dock/bank_account_service/account/AccountsControllerTest.java
+++ src/test/java/br/com/dock/bank_account_service/account/AccountsControllerTest.java
@@ -2,6 +2,7 @@ package br.com.dock.bank_account_service.account;
 
 import br.com.dock.bank_account_service.account.cases.BlockAccount;
 import br.com.dock.bank_account_service.account.cases.CreateNewAccount;
+import br.com.dock.bank_account_service.account.cases.FindAccountStatement;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -20,6 +21,7 @@ class AccountsControllerTest {
     public static final String PATH_ACCOUNTS_RESOURCE = "/accounts";
     public static final String VERSION_1 = "v1";
     public static final String PATH_BLOCKS = "/blocks";
+    public static final Long ACCOUNT_ID = 1L;
 
     @Autowired
     WebTestClient client;
@@ -41,14 +43,25 @@ class AccountsControllerTest {
     @DisplayName("Should block an account")
     @ParameterizedTest(name = "{index} {0}")
     @MethodSource("br.com.dock.bank_account_service.account.cases.BlockAccount#parametersBlockAccount")
-    void testShouldBlockAccount(String title, BlockAccount.UserCase userCase) {
-        var accountId = 1;
-
-        client.patch().uri(PATH_ACCOUNTS_RESOURCE + "/" + accountId + PATH_BLOCKS)
+    void testShouldBlockAnAccount(String title, BlockAccount.UserCase userCase) {
+        client.patch().uri(PATH_ACCOUNTS_RESOURCE + "/" + ACCOUNT_ID + PATH_BLOCKS)
                 .header(HEAD_ACCEPT_VERSION, VERSION_1)
                 .header(HEAD_CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .bodyValue(userCase.getRequest())
                 .exchange()
                 .expectStatus().isEqualTo(userCase.getHttpStatus());
+    }
+
+    @DisplayName("Should find the account statement")
+    @ParameterizedTest(name = "{index} {0}")
+    @MethodSource("br.com.dock.bank_account_service.account.cases.FindAccountStatement#parametersFindAccountStatement")
+    void testShouldFindTheAccountStatement(String title, FindAccountStatement.UserCase userCase) {
+        client.get().uri(PATH_ACCOUNTS_RESOURCE + "/" + ACCOUNT_ID)
+                .header(HEAD_ACCEPT_VERSION, VERSION_1)
+                .header(HEAD_CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .exchange()
+                .expectStatus().isEqualTo(userCase.getHttpStatus())
+                .expectBody()
+                .json(userCase.getResponse());
     }
 }
