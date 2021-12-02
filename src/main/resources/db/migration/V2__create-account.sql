c30ab12b589ddf4c6b689bc7e6800140f0689527
andferreira <anderson.ferreira@mercadolivre.com>
2021-12-01 21:16:55 -0300

    feat: Add the configurations to run the application with docker compose

--- src/main/resources/db/migration/V2__create-account.sql
+++ src/main/resources/db/migration/V2__create-account.sql
@@ -6,9 +6,6 @@ create table Account
     withdrawalDayLimit decimal(9, 2) not null,
     flagActive         boolean       not null,
     accountType        int           not null,
-    createdAt          datetime      not null
+    createdAt          datetime      not null,
+    foreign key (idPerson) references Person (idPerson)
 );
-
-alter table Person
-    add constraint fk_account_people
-        foreign key (idPerson) references Person (idPerson);
--- src/main/resources/db/migration/V3__create-transaction.sql
+++ src/main/resources/db/migration/V3__create-transaction.sql
@@ -3,11 +3,8 @@ create table Transaction
     idTransaction int primary key auto_increment,
     idAccount     int           not null,
     amount        decimal(9, 2) not null,
-    createdAt     datetime
+    createdAt     datetime,
+    foreign key (idAccount) references Account (idAccount)
 );
-
-alter table Transaction
-    add constraint fk_transaction_account
-        foreign key (idAccount) references Account (idAccount);
 
 create index index_transaction_createAt on transaction (createdAt);
